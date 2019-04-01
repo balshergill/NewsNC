72635a89a5b334b2bfa8bc769bea82cb92cb74be
balshergill <baldipkshergill@gmail.com>
2019-04-01 19:47:57 +0100

    set up the index.js files for the data

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,1 +1,6 @@
-// require in and export out all test data
+const articles = require("./articlesData");
+const comments = require("./commentsData");
+const topics = require("./topicsData");
+const users = require("./usersData");
+
+module.exports = { articles, comments, topics, users };
