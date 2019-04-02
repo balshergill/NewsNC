13db0bba653a507eb644d88ba2c0dccc094865d0
balshergill <baldipkshergill@gmail.com>
2019-04-02 21:02:01 +0100

    Created createKeyPairs reference function

--- utils/utilsFunctions.js
+++ utils/utilsFunctions.js
@@ -1,6 +1,13 @@
 exports.timeStampJSConversion = arr => {
   return arr.map(article => {
     const newTime = new Date(article.created_at);
-    return { ...article, created_at: newTime };
+    const sliced = newTime.toISOString().slice(0, 10);
+    return { ...article, created_at: sliced };
   });
 };
+
+exports.createKeyPairs = articleArr =>
+  articleArr.reduce((acc, article) => {
+    acc[article.title] = article.article_id;
+    return acc;
+  }, {});
