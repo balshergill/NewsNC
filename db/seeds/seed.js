2198b1607601e81b85f852a0529a26d261c13e9d
balshergill <baldipkshergill@gmail.com>
2019-04-02 09:39:47 +0100

    Seed file created - initial commit. Conversion functions need to be added

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,38 @@
-// const {  } = require('../data');
+const {
+  topicsData,
+  usersData,
+  articlesData,
+  commentsData
+} = require("../data");
+
+const knex = require("knex");
 
 exports.seed = (knex, Promise) => {
   return knex.migrate
     .rollback()
     .then(() => knex.migrate.latest())
     .then(() => {
-      // insert data
+      return knex("topics")
+        .insert(topicsData)
+        .into("topics")
+        .returning("*");
+    })
+    .then(() => {
+      return knex("users")
+        .insert(usersData)
+        .into("users")
+        .returning("*");
+    })
+    .then(() => {
+      return knex("articles")
+        .insert(articlesData)
+        .into("articles")
+        .returning("*");
+    })
+    .then(() => {
+      return knex("comments")
+        .insert(commentsData)
+        .into("comments")
+        .returning("*");
     });
 };
