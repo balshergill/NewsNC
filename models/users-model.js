58327151b90a474798ff68fc7be55e818af339bc
balshergill <baldipkshergill@gmail.com>
2019-04-04 16:14:31 +0100

    updated users-model to remove comments

--- models/users-model.js
+++ models/users-model.js
@@ -4,8 +4,4 @@ exports.getOneUser = username => {
     .select("username", "avatar_url", "name")
     .from("users")
     .where("users.username", "=", username);
-
-  // .leftJoin("comments", "users.article_id", "comments.article_id")
-  // .count("comments.article_id as comment_count")
-  // .groupBy("users.article_id");
 };
