bc22d03893937b79f61603774df056874ec784f9
balshergill <baldipkshergill@gmail.com>
2019-04-04 15:08:17 +0100

    comments-model created and added function for patch

---
+++ models/comments-model.js
@@ -0,0 +1,12 @@
+const knexConnection = require("../db/connection");
+
+exports.updateComment = (id, voteIncrement) => {
+  return knexConnection("comments")
+    .from("comments")
+    .where("comment_id", "=", id)
+    .leftJoin("comments", "articles.article_id", "comments.article_id")
+    .count("comments.article_id AS comment_count")
+    .groupBy("comments.article_id")
+    .increment("votes", voteIncrement)
+    .returning("*");
+};
