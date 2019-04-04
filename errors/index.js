f3d2297cda10ca5549d9d357ff1eb1b7d76e8da6
balshergill <baldipkshergill@gmail.com>
2019-04-04 17:01:26 +0100

    Added error functions to errors/index/js

--- errors/index.js
+++ errors/index.js
@@ -1,11 +1,31 @@
-exports.routeNotFound = (req, res) => {
-  res.status(404).send({ msg: 'Route Not Found' });
-};
+exports.handle400((err, req, res, next) => {
+  const badRequestCodes = ['22P02', ...];
+  if (badRequestCodes.includes(err.code)) {
+    res.status(400).send({ msg: err.message || 'Bad Request' });
+  } else next(err);
+});
 
-exports.methodNotAllowed = (req, res) => {
-  res.status(405).send({ msg: 'Method Not Allowed' });
-};
+exports.handle404 = ((err, req, res, next) => {
+  if (err.status === 404) res.status(404).send({ msg: err.message });
+  else {
+    next(err);
+  }
+});
 
-exports.handle500 = (err, req, res, next) => {
-  res.status(500).send({ msg: 'Internal Server Error' });
-};
+exports.handle405 = ((err, req, res, next) => {
+  if (err.status === 405) res.status(405).send({ msg: err.message });
+  else {
+    next(err);
+  }
+});
+
+exports.handle422 = ((err, req, res, next) => {
+  if (err.status === 422) res.status(422).send({ msg: err.message });
+  else {
+    next(err);
+  }
+});
+
+exports.handle500 = ((err, req, res, next) => {
+  res.status(500).send({ msg: "Internal Server Error" });
+});
