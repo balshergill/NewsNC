b3b66490690a06c096ee80c0efad216853ca4e23
balshergill <baldipkshergill@gmail.com>
2019-04-04 15:10:42 +0100

    updated commentsRouter to add delete route

--- routes/commentsRouter.js
+++ routes/commentsRouter.js
@@ -1,6 +1,9 @@
 const commentsRouter = require("express").Router();
 const { voteOnComment } = require("../controllers/comments-controller");
 
-commentsRouter.route("/comments/:comment_id").patch(voteOnComment);
+commentsRouter
+  .route("/comments/:comment_id")
+  .patch(voteOnComment)
+  .delete(deleteOneComment);
 
 module.exports = commentsRouter;
