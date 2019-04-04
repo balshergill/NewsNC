120210760ba70a0953aca7a233748a2550ad758b
balshergill <baldipkshergill@gmail.com>
2019-04-04 15:22:33 +0100

    Added deleteOneComment function to comments-controller

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -1,4 +1,4 @@
-const { updateComment } = require("../models/comments-model");
+const { updateComment, removeComment } = require("../models/comments-model");
 
 exports.voteOnComment = (req, res, next) => {
   const id = req.params.comment_id;
@@ -6,6 +6,15 @@ exports.voteOnComment = (req, res, next) => {
   return updateComment(id, vote)
     .then(([updatedComment]) => {
       res.status(200).send({ updatedComment });
+    })
+    .catch(next);
+};
+
+exports.deleteOneComment = (req, res, next) => {
+  const id = req.params.comment_id;
+  return removeComment(id)
+    .then(comment => {
+      res.status(204).json({ comment });
     })
     .catch(next);
 };
