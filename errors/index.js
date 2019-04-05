037bdb83c9592b5a36546dc3013df934a8bfa915
balshergill <baldipkshergill@gmail.com>
2019-04-05 09:20:53 +0100

    Updated error functions to change the order and messages on errors

--- errors/index.js
+++ errors/index.js
@@ -1,31 +1,28 @@
-exports.handle400((err, req, res, next) => {
-  const badRequestCodes = ['22P02', ...];
+exports.methodNotAllowed = (req, res) => {
+  res.status(405).send({ msg: "Method Not Allowed" });
+};
+
+exports.handle400 = (err, req, res, next) => {
+  const badRequestCodes = ["22P02"];
   if (badRequestCodes.includes(err.code)) {
-    res.status(400).send({ msg: err.message || 'Bad Request' });
+    res.status(400).send({ msg: "Bad Request" });
   } else next(err);
-});
+};
 
-exports.handle404 = ((err, req, res, next) => {
-  if (err.status === 404) res.status(404).send({ msg: err.message });
+exports.handle404 = (err, req, res, next) => {
+  if (err.status === 404) res.status(404).send({ msg: "Resource Not Found" });
   else {
     next(err);
   }
-});
+};
 
-exports.handle405 = ((err, req, res, next) => {
-  if (err.status === 405) res.status(405).send({ msg: err.message });
+exports.handle422 = (err, req, res, next) => {
+  if (err.status === 422) res.status(422).send({ msg: "Unprocessable Entity" });
   else {
     next(err);
   }
-});
+};
 
-exports.handle422 = ((err, req, res, next) => {
-  if (err.status === 422) res.status(422).send({ msg: err.message });
-  else {
-    next(err);
-  }
-});
-
-exports.handle500 = ((err, req, res, next) => {
+exports.handle500 = (err, req, res, next) => {
   res.status(500).send({ msg: "Internal Server Error" });
-});
+};
