de4856dbb80343d52b2bbf9caf0698dda3140d12
balshergill <baldipkshergill@gmail.com>
2019-04-18 15:26:21 +0100

    added Knex file

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+const ENV = process.env.NODE_ENV || "development";
+
+const baseConfig = {
+  client: "pg",
+  migrations: {
+    directory: "./db/migrations"
+  },
+  seeds: {
+    directory: "./db/seeds"
+  }
+};
+
+const customConfigs = {
+  development: {
+    connection: {
+      database: "ncnews"
+    }
+  },
+  test: {
+    connection: {
+      database: "ncnews_test"
+    }
+  },
+  production: {
+    connection: `$process.env.DATABASE_URL}ssl=true`
+  }
+};
+
+module.exports = { ...baseConfig, ...customConfigs[ENV] };
